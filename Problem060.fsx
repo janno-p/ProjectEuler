728c4e2027c6fc96ec3a6dbb5ece9ae7d55af672
Janno Põldma <janno.poldma@gmail.com>
2014-08-17 08:50:21 +0300

    Working on a problem.

--- Problem060.fsx
+++ Problem060.fsx
@@ -10,5 +10,70 @@
  *  another prime.
  *)
 
+open System
+open System.Collections.Generic
+
 module Problem060 =
+    type Primo () =
+        let knownPrimes = new SortedSet<int>()
+
+        let mutable lastValue = 0
+
+        let testPrime (n : int) =
+            let max = Math.Sqrt(float n) |> int
+            let rec test (e : IEnumerator<int>) =
+                if e.MoveNext() then
+                    let p = e.Current
+                    if p > max then
+                        true
+                    elif n % p = 0 then
+                        false
+                    else
+                        test e
+                else
+                    true
+            test (knownPrimes.GetEnumerator())
+
+        member this.Next
+            with get () =
+                let rec next cur =
+                    let n = cur + 1
+                    match testPrime n with
+                    | true -> knownPrimes.Add n |> ignore
+                              n
+                    | _ -> next n
+                match knownPrimes.Count with
+                | 0 -> knownPrimes.Add 2 |> ignore
+                       2
+                | _ -> next knownPrimes.Max
+
+        member this.IsPrime n =
+            match n with
+            | x when x > knownPrimes.Max -> false
+            | _ -> knownPrimes.Contains(n)
+
+    let splits n =
+        let rec split r = seq {
+            let nr = r * 10
+            if nr < n then
+                match n % nr with
+                | x when x < r -> ()
+                | x -> yield (n / nr, x)
+                yield! split nr
+        }
+        split 1
+
+
+    let primo = new Primo()
+    let rec proov () =
+        match primo.Next with
+        | x when x > 10000 -> ()
+        | x -> ()
+
+    [ 0 .. 9 ] |> Seq.iter (fun _ -> printfn "%d" primo.Next)
+    printfn "2 is prime: %b" (primo.IsPrime 2)
+    printfn "3 is prime: %b" (primo.IsPrime 3)
+
+    splits 1709 |> Seq.iter (fun (x, y) -> printfn "(%d, %d)" x y)
+
     ()
